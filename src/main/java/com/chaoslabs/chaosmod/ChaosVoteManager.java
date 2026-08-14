5ba96087afd13a9d23b2c77b908a59b402aa968c
voken99991 <jurbergsd@gmail.com>
2026-08-14 23:07:42 +0100

    Add files via upload

--- src/main/java/com/chaoslabs/chaosmod/ChaosVoteManager.java
+++ src/main/java/com/chaoslabs/chaosmod/ChaosVoteManager.java
@@ -11,8 +11,6 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.mob.Mob;
-import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.ClickEvent;
